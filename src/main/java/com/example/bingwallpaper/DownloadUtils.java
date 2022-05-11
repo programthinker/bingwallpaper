eb53c561b9dd693b7f404dedf3d0d8cedbc91c45
ZhangGeYang <thinkingprogramer@gmail.com>
2022-05-11 23:02:09 +0800

    使用并行流修改下载方式

--- src/main/java/com/example/bingwallpaper/DownloadUtils.java
+++ src/main/java/com/example/bingwallpaper/DownloadUtils.java
@@ -6,6 +6,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -36,18 +37,20 @@ public class DownloadUtils {
 
         List<String> strings = Files.readAllLines(readmePath, StandardCharsets.UTF_8);
 
-        for (String string : strings) {
-            if (!Objects.isNull(string)){
-                String name = string.split("&&")[0];
-                String url = string.split("&&")[1];
-                HttpGet httpGet = new HttpGet(url);
-                CloseableHttpResponse httpResponse = HttpClientBuilder.create().build().execute(httpGet);
+        strings.parallelStream().forEach(s -> {
+            String name = s.split("&&")[0];
+            String url = s.split("&&")[1];
+            HttpGet get = new HttpGet(url);
+            try {
+                CloseableHttpResponse httpResponse = HttpClientBuilder.create().build().execute(get);
                 InputStream content = httpResponse.getEntity().getContent();
                 Path path = Paths.get(downloadLocation + name);
                 Files.copy(content, path, StandardCopyOption.REPLACE_EXISTING);
                 IOUtils.closeQuietly(content);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        }
+        });
     }
 
     public static void main(String[] args) throws Exception {
